9e379a29c9654fd12d482118b4ec51a21475a097
MatCha <43952141+1712190521@users.noreply.github.com>
2019-07-30 19:14:10 +0800

    Add files via upload

---
+++ 头文件.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize(2)
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<cmath>
+#include<set>
+#include<functional>
+#include<algorithm>
+#include<cassert>
+#include<map>
+#include<stack>
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rep2(i,a,b) for(int i=a;i>=b;i--)
+typedef long long ll;
+using namespace std;
+const int N = 5000;
+const int mod = 998244353;
+const int  INF = 0x3f3f3f3f;
+const ll inf=1ll<<62-1ll;
+inline void read(int& x) {
+	x = 0; char c = getchar();
+	while (c > '9' || c < '0') c = getchar();
+	while (c >= '0' && c <= '9') x = x * 10 + c - '0', c = getchar();
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+#ifdef ONLINE_JUDGE
+#else
+	freopen("./std.in", "r", stdin);
+#endif
+	read(n); read(m); read(s); read(t);
+	return 0;
+}
